4c75794180e9911c47d2c8907a2b5516f6c0e3e8
carlosgsm <45521638+carlosgsm@users.noreply.github.com>
2019-05-03 21:20:20 +0200

    comit viernes 3

--- project2/controllers/search.controller.js
+++ project2/controllers/search.controller.js
@@ -2,16 +2,22 @@ const createError = require('http-errors')
 const mongoose = require('mongoose')
 const Player = require('../models/player.model');
 
-module.exports.list = (req, res, next) => {
-  const search = req.query;
-  Player.find({$or:[{nick:search.nick}, {nickInGame: search.nickInGame},
-  {country:search.country},{game:search.game}]})
+module.exports.list = ((req, res, next) => {
+//   const search = req.query;
+//   Player.find({$or:[{nick:search.nick}, {nickInGame: search.nickInGame},
+//   {country:search.country},{game:search.game}]})
 
-    .then( players => {
-      res.render('search/searchResults', {players})
-    })
-    .catch(error => next(error))
-}
+//     .then( players => {
+//       res.render('search/searchResults', {players})
+//     })
+//     .catch(error => next(error))
+// }
+Player.find({})
+.then( players => {
+  res.render('search/searchResults', {players})
+})
+.catch(error => next(error))
+})
 
 module.exports.searchForm = (req, res, next) => {
     
